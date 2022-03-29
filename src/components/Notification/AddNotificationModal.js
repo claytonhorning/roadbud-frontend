697e961639b24b786a16d2b97a40b9529f96263f
Clayton Horning <clayton.horning@colorado.edu>
2022-03-29 17:25:06 -0600

    Add notification modal chip options with state

--- src/components/Notification/AddNotificationModal.js
+++ src/components/Notification/AddNotificationModal.js
@@ -7,10 +7,57 @@ import {
     KeyboardAvoidingView,
     ScrollView,
 } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import RouteInput from '../../components/Inputs/RouteInput'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default function AddNotificationModal({ visible }) {
+    const [selectedChips, setSelectedChips] = useState([])
+    const [unselectedChips, setUnselectedChips] = useState(['CDOT', 'Roadbud'])
+
+    const handleChipPressed = (name) => {
+        if (selectedChips.includes(name)) {
+            setSelectedChips(
+                selectedChips.filter((namePassed) => name !== namePassed)
+            )
+            setUnselectedChips([...unselectedChips, name])
+        } else {
+            setUnselectedChips(
+                unselectedChips.filter((namePassed) => name !== namePassed)
+            )
+            setSelectedChips([...selectedChips, name])
+        }
+    }
+    console.log(unselectedChips)
+
+    unselectedChips.map((x) => console.log(x))
+
+    const AddChip = ({ name }) => (
+        <TouchableOpacity
+            onPress={() => handleChipPressed(name)}
+            style={styles.chip}
+        >
+            <Text style={{ color: '#fff', fontWeight: '600' }}>{name}</Text>
+            <Icon
+                style={{ color: 'green', fontSize: 16, marginLeft: 5 }}
+                name="plus-circle"
+            />
+        </TouchableOpacity>
+    )
+
+    const DeleteChip = ({ name }) => (
+        <TouchableOpacity
+            onPress={() => handleChipPressed(name)}
+            style={styles.chip}
+        >
+            <Text style={{ color: '#fff', fontWeight: '600' }}>{name}</Text>
+            <Icon
+                style={{ color: 'red', fontSize: 16, marginLeft: 5 }}
+                name="minus-circle"
+            />
+        </TouchableOpacity>
+    )
+
     return (
         <Modal visible={visible} animationType="slide" transparent={true}>
             <ScrollView contentContainerStyle={styles.container}>
@@ -25,15 +72,27 @@ export default function AddNotificationModal({ visible }) {
                                 this route.
                             </Text>
                             <RouteInput />
+                            <View style={{ flexDirection: 'row' }}>
+                                {selectedChips.map((chip) => (
+                                    <DeleteChip key={chip} name={chip} />
+                                ))}
+                            </View>
                             <Text
                                 style={{
                                     fontSize: 16,
                                     fontWeight: '600',
-                                    marginTop: 15,
+                                    marginTop: 5,
                                 }}
                             >
                                 Click to add or remove
                             </Text>
+
+                            <View style={{ flexDirection: 'row' }}>
+                                {unselectedChips.map((chip) => (
+                                    <AddChip key={chip} name={chip} />
+                                ))}
+                            </View>
+
                             <TouchableOpacity style={styles.addButton}>
                                 <Text
                                     style={{
@@ -78,6 +137,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 5,
         backgroundColor: '#FD7A22',
+        marginVertical: 10,
+    },
+    chip: {
+        backgroundColor: '#000',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 15,
+        paddingRight: 10,
+        paddingVertical: 3,
+        borderRadius: 20,
+        marginRight: 10,
         marginVertical: 10,
     },
 })
